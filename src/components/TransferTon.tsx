247d08f68444a1a54685336e601b6525b444b524
monkey <2521546500@qq.com>
2024-07-24 10:49:06 +0800

    add comment

--- src/components/TransferTon.tsx
+++ src/components/TransferTon.tsx
@@ -1,51 +1,64 @@
-import { useState } from "react";
-import styled from "styled-components";
-import { Address, toNano } from "ton";
-import { useTonConnect } from "../hooks/useTonConnect";
-import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from "./styled/styled";
+import { useState } from 'react'
+import styled from 'styled-components'
+import { Address, comment, toNano } from 'ton'
+import { useTonConnect } from '../hooks/useTonConnect'
+import { Card, FlexBoxCol, FlexBoxRow, Button, Input } from './styled/styled'
 
 export function TransferTon() {
-  const { sender, connected } = useTonConnect();
+    const { sender, connected } = useTonConnect()
 
-  const [tonAmount, setTonAmount] = useState("0.01");
-  const [tonRecipient, setTonRecipient] = useState(
-    "EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c"
-  );
+    const [text, settext] = useState('')
+    const [tonAmount, setTonAmount] = useState('0.01')
+    const [tonRecipient, setTonRecipient] = useState(
+        'EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c'
+    )
 
-  return (
-    <Card>
-      <FlexBoxCol>
-        <h3>Transfer TON</h3>
-        <FlexBoxRow>
-          <label>Amount </label>
-          <Input
-            style={{ marginRight: 8 }}
-            type="number"
-            value={tonAmount}
-            onChange={(e) => setTonAmount(e.target.value)}
-          ></Input>
-        </FlexBoxRow>
-        <FlexBoxRow>
-          <label>To </label>
-          <Input
-            style={{ marginRight: 8 }}
-            value={tonRecipient}
-            onChange={(e) => setTonRecipient(e.target.value)}
-          ></Input>
-        </FlexBoxRow>
-        <Button
-          disabled={!connected}
-          style={{ marginTop: 18 }}
-          onClick={async () => {
-            sender.send({
-              to: Address.parse(tonRecipient),
-              value: toNano(tonAmount),
-            });
-          }}
-        >
-          Transfer
-        </Button>
-      </FlexBoxCol>
-    </Card>
-  );
+    return (
+        <Card>
+            <FlexBoxCol>
+                <h3>Transfer TON</h3>
+                <FlexBoxRow>
+                    <label>Amount </label>
+                    <Input
+                        style={{ marginRight: 8 }}
+                        type="number"
+                        value={tonAmount}
+                        onChange={(e) => setTonAmount(e.target.value)}
+                    ></Input>
+                </FlexBoxRow>
+                <FlexBoxRow>
+                    <label>To </label>
+                    <Input
+                        style={{ marginRight: 8 }}
+                        value={tonRecipient}
+                        onChange={(e) => setTonRecipient(e.target.value)}
+                    ></Input>
+                </FlexBoxRow>
+
+                {/* Comment */}
+                <FlexBoxRow>
+                    <label>Comment </label>
+                    <Input
+                        style={{ marginRight: 8 }}
+                        value={tonRecipient}
+                        onChange={(e) => settext(e.target.value)}
+                    ></Input>
+                </FlexBoxRow>
+
+                <Button
+                    disabled={!connected}
+                    style={{ marginTop: 18 }}
+                    onClick={async () => {
+                        sender.send({
+                            to: Address.parse(tonRecipient),
+                            value: toNano(tonAmount),
+                            body: comment(text)
+                        })
+                    }}
+                >
+                    Transfer
+                </Button>
+            </FlexBoxCol>
+        </Card>
+    )
 }
